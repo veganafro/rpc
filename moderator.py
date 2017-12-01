a94ae2289a1dd6130491ab36f9741563a5983e65
jeremymuhia <jmuhia80@gmail.com>
2017-11-30 23:47:06 -0500

    build topic and blah_run parser

--- moderator.py
+++ moderator.py
@@ -8,6 +8,16 @@ def run():
     direction = sys.argv[1]
     if direction == "answer":
         print(answer(" ".join(sys.argv[2:])))
+    elif direction == "elaborate":
+        topic = get_topic(sys.argv[2:])
+        blah_run = list(map(
+            int,
+            filter(
+                lambda x: x.isdigit(),
+                sys.argv
+            )
+        ))
+        print(topic, blah_run)
 
 
 def answer(question):
@@ -22,6 +32,19 @@ def answer(question):
 def elaborate(topic, blah_run):
     channel = grpc.insecure_channel("localhost:50050")
     stub = debate_pb2_grpc.CandidateStub(channel)
+    reply = stub.Elaborate(
+        debate_pb2.ElaborateRequest(topic=topic, blah_run=blah_run)
+    )
+    return reply.answer
+
+
+def get_topic(args):
+    ending_index = 0
+    while ending_index < len(args):
+        if args[ending_index].isdigit():
+            return " ".join(args[:ending_index])
+        ending_index += 1
+    return " ".join(args[:ending_index])
 
 
 if __name__ == "__main__":
