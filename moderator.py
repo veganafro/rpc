a26896532604b7d5dd1d98d199cda6cc6c9a07e5
jeremymuhia <jmuhia80@gmail.com>
2017-11-30 22:23:24 -0500

    building client

--- moderator.py
+++ moderator.py
@@ -1,1 +1,14 @@
 import sys
+
+import grpc
+import debate_pb2
+import debate_pb2_grpc
+
+def run():
+    direction = sys.argv[1]
+    if direction is "answer":
+        print(answer(sys.argv[2]))
+
+def answer(question):
+    channel = grpc.insecure_channel("localhost:50050")
+    stub = debate_pb2_grpc.
