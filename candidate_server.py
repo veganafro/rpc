6b11939f7d814c7d2a30ea3c0198549090d218c2
jeremymuhia <jmuhia80@gmail.com>
2017-11-30 21:44:57 -0500

    build answer in candidate server

--- candidate_server.py
+++ candidate_server.py
@@ -5,7 +5,9 @@ import debate_pb2_grpc
 import consultation_pb2
 import consultation_pb2_grpc
 
+import time
 from random import randint
+from concurrent import futures
 
 interrogatives = set(["why", "what", "how", "who", "when"])
 
@@ -21,7 +23,7 @@ class Candidate(debate_pb2_grpc.CandidateServicer):
                 "your 3 cent titanium tax doesn't go too far enough"
             ]
             return debate_pb2.AnswerReply(answer=replies[randint(0, 1)])
-        for idx, word in question:
+        for idx, word in enumerate(question):
             if word is "you":
                 question[idx] = "I"
             elif word is "your":
@@ -34,3 +36,19 @@ class Candidate(debate_pb2_grpc.CandidateServicer):
         return debate_pb2.AnswerReply(
             answer="You asked me " + " ".join(question) + " but I want to say that" + retort.retort
         )
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    debate_pb2_grpc.add_CandidateServicer_to_server(Candidate(), server)
+    server.add_insecure_port("[::]:50050")
+    server.start()
+    try:
+        while True:
+            time.sleep(60 * 60 * 24)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == "__main__":
+    serve()
